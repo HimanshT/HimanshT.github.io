fc677a1e8a5b76360009a541258138cc4ebee652
HimanshT <himanshutripathi4462@gmail.com>
2021-12-12 18:10:07 +0530

    commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 
 // Mongoose Connection
 mongoose.connect('mongodb://localhost:27017/yourTrip',
@@ -21,15 +22,48 @@ db.once("open", () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+//to parse the body
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 //routers
 app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/check', async (req, res) => {
-    const camp = new Campground({ title: 'My hoste' });
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });//we find the 
+    //campgrounds and then pass it through the ejs file to render them
+})
+
+// for making a new campground
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+//for saving new values
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save()
+    res.redirect(`campgrounds/${campground._id}`);
+})
+
+//for individual campgrounds
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+})
+//editing a campground
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+})
+//put request to update the campground
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`${campground._id}`); // res.redirect('/campgrounds/${campground._id}) is not working;
 })
 
 app.listen(3000, () => {
