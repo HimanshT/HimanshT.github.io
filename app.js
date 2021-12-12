f6f048aeff82d8bd295d1fdf8b9fbf9a2f31554a
HimanshT <himanshutripathi4462@gmail.com>
2021-12-13 02:00:48 +0530

    commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 
@@ -19,6 +20,7 @@ db.once("open", () => {
 });
 
 //views connection
+app.engine('ejs', ejsMate) //we are telling express to use ejsMate
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -64,6 +66,12 @@ app.put('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`${campground._id}`); // res.redirect('/campgrounds/${campground._id}) is not working;
+})
+//to delete the campground
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 })
 
 app.listen(3000, () => {
