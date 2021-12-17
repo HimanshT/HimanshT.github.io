319979dbabbea5b99850291e48a53f0fdd8fa627
HimanshT <himanshutripathi4462@gmail.com>
2021-12-18 02:43:59 +0530

    added star rating

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,66 +4,21 @@ const catchAsync = require('../utils/catchAsync');//for async funtion
 const Campground = require('../models/campground');
 const Joi = require('joi');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+//controller routes
+const campgrounds = require('../controllers/campgrounds')
 
-router.get('/', catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });//we find the 
-    //campgrounds and then pass it through the ejs file to render them
-}))
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
-// for making a new campground
-router.get('/new', isLoggedIn, (req, res) => {
-    res.render('campgrounds/new');
-})
+router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
-//for saving new values
-router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
-    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data');
-    const campground = new Campground(req.body.campground);
-    campground.author = req.user._id;
-    await campground.save();
-    req.flash('success', 'Successfully made a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
-//for individual campgrounds
-router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate({
-        path: 'reviews',
-        populate: {
-            path: 'author'
-        }
-    }).populate('author');//populate is used to show reviws on main page
-    console.log(campground)
-    if (!campground) {
-        req.flash('error', 'Sorry,campground not available');
-        res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/show', { campground });
-}))
-//editing a campground
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground) {
-        req.flash('error', 'sorry! couldnt find it');
-        res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/edit', { campground });
-}))
-//put request to update the campground
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    req.flash('success', "Successfully Updated");
-    res.redirect(`/campgrounds/${campground._id}`); // res.redirect('/campgrounds/${campground._id}) is not working;
-}))
-//to delete the campground
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    req.flash('success', ' Campground Deleted')
-    res.redirect('/campgrounds');
-}))
+
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
 module.exports = router;
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,31 +1,12 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync');//for async funtion
-const Campground = require('../models/campground');
-const Review = require('../models/review');
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
+const reviews = require('../controllers/reviews');
 //middleware of joi -->review
 //this is done to prevent it's data extraction from postman
-
-//for submitting reviews of campground
-router.post('/', validateReview, isLoggedIn, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Review');
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-//for deleting the reviews of campground
+router.post('/', validateReview, isLoggedIn, catchAsync(reviews.createReview))
 //pull operator is to delete the review of that campground
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success', ' Review Deleted')
-    res.redirect(`/campgrounds/${id}`);
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports = router;
